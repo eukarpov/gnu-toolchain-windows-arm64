9a4338ec7821241559af2fe4bd60b1c38c6bf20b
Evgeny Karpov <evgeny@kmaps.co>
2025-10-27 07:44:12 +0000

    Add support for empty toolchain keys when packing is not required

--- .github/scripts/toolchain/pack.sh
+++ .github/scripts/toolchain/pack.sh
@@ -4,7 +4,13 @@ source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
 echo "::group::Pack toolchain"
     mkdir -p $ARTIFACT_PATH
-    tar czf $ARTIFACT_PATH/$TOOLCHAIN_PACKAGE_NAME -C $TOOLCHAIN_PATH .
+
+    if [ "$PACK_TOOLCHAIN" = "true" ]; then
+        tar czf $ARTIFACT_PATH/$TOOLCHAIN_PACKAGE_NAME -C $TOOLCHAIN_PATH .
+    else
+        tar czf $ARTIFACT_PATH/$TOOLCHAIN_PACKAGE_NAME --files-from /dev/null
+    fi
+
 echo "::endgroup::"
 
 echo 'Success!'
