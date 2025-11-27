97bd6e4072a76c771ee71bdda53805395656ed7e
Evgeny Karpov <evgeny@kmaps.co>
2025-11-27 19:08:19 +0100

    Configure toolchain file with Release as default build type

--- .github/scripts/tests/build.sh
+++ .github/scripts/tests/build.sh
@@ -8,8 +8,13 @@ if [[ "$RUN_CONFIG" = 1 ]] || [[ ! -f "build/Makefile" ]]; then
   echo "::group::Configure AArch64 Tests"
     rm -rf build
 
+    if [[ "$DEBUG" = 1 ]]; then
+        HOST_OPTIONS="$HOST_OPTIONS \
+            -DCMAKE_BUILD_TYPE=Debug"
+    fi
+
     if [[ "$UPSTREAM_VALIDATION" = 1 ]]; then
-        HOST_OPTIONS=" \
+        HOST_OPTIONS="$HOST_OPTIONS \
             -DUPSTREAM_VALIDATION=1"
     fi
 
